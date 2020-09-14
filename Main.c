c48c94f97b51d0256f3f10fdb6e69fc4a24e3f08
Angel Gomez <angel.gomez1394@alumnos.udg.mx>
2020-09-14 17:25:13 -0500

    Capturamos la edad e imprimimos la edad +1

--- Main.c
+++ Main.c
@@ -3,10 +3,14 @@
 
 int main(){
     char nombre[20];
+    unsigned int edad;
 
     printf("Hola Mundo!\n");
     printf("Ingrese su nombre\n");
     fgets(nombre, sizeof(nombre),stdin);
     printf("Buenos Dias: %s\n", nombre);
+    printf("Ingrese su edad\n");
+    scanf("%u", &edad);
+    printf("Cumpliras %u", edad+1);
     return 0;
 }
